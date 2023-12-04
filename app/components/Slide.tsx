76770094a6ad6cc652f5ed626523c6d71cf5496d
Alessio Russo <16mmstudios@gmail.com>
2023-12-04 14:39:47 +0100

    slider componenent

---
+++ app/components/Slide.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import {motion, Variant } from 'framer-motion'
+import img from '../assets/blue_wave__abstract_picture_walpaper_amazing.jpg'
+import Image from 'next/image'
+function Slide() {
+
+    const divsProva = [ "1","1", "1","1","1","1","1","4","1","2","1","1","1","5","1","1","1","1",]
+
+    const variant = {
+        initial:{
+            x:0
+        },
+        animate:{
+            x:"-220%",
+            transition:{
+                ease: "linear",
+                 duration: 14,
+                 repeat: Infinity
+            },
+            whileHover:{
+                transition:{
+                    duration:0
+                },
+                scale:1,
+            }
+        },
+    }
+
+  return (
+    <motion.div className=' w-full flex gap-2 text-9xl h-80' variants={variant} initial="initial" animate="animate" >
+         {divsProva.map((itm ,indx)=>(
+        <Image key={indx} src={img} alt='' className=' w-full text-9xl bg-red-500'></Image> 
+        ))} 
+        {/* slide slide slide slide slide slide slide slide slide slide slide slide slide slide slide slide slide slide slide slide slide slide slide slide slide slide slide slide slide slide */}
+     </motion.div>
+  )
+}
+
+export default Slide
--- app/pages/home/page.tsx
+++ app/pages/home/page.tsx
@@ -2,11 +2,12 @@
 import React from 'react'
 import { HomeContainer } from '@/app/styles/HomePage.style'
 import ParApp from '@/app/components/Parallax'
+import Slide from '@/app/components/Slide'
 
 export default function HomePage() {
   return (
-    <HomeContainer>
-     
+    <HomeContainer className=' overflow-hidden'>
+      <Slide/>
     </HomeContainer> 
   )
 }
