2266b07d5c9b963a58bfcf29bc7dbd182bfa5264
n0wherefast <97000096+n0wherefast@users.noreply.github.com>
2023-12-12 13:11:16 +0100

    aggiunto background home

--- app/pages/home/page.tsx
+++ app/pages/home/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React from 'react'
-import { HomeContainer,ImageContainer } from '@/app/styles/HomePage.style'
+import { HomeContainer,ImageContainer ,DiscoverContainer} from '@/app/styles/HomePage.style'
 import Slide from '@/app/components/Slide'
 import Image from 'next/image'
 import img from '../../assets/29-09-11.jpg'
@@ -8,39 +8,7 @@ import styled from 'styled-components'
 import { StaticImageData } from 'next/image'
 import '../../globals.css'
 
-export const DiscoverContainer = styled.button`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-bottom: 13rem;
-  margin-left: 3rem;
-  font-size:10rem;
-  font-weight:900;
-  font-style: italic;
-  text-transform: uppercase;
-  /* backdrop-filter: blur(10px); */
-  height:10rem;
-  @media (max-width:1000px) {
-    
-  }
-  @media (max-width:700px) {
-  border: 2px solid;
-  border-radius: 10px;
-  margin-bottom: -20rem;
-  padding: 10px;
-  height: 6rem;
-  backdrop-filter: blur(20px);
-  margin-left:0px;
-  font-size:4rem;
-  font-weight:900;
-  }
 
-  color:rgb(251 191 36);
-  &:hover{
-    color: darkorange;
-  }
-
-`
 
 
 export default function HomePage() {
--- app/styles/HomePage.style.ts
+++ app/styles/HomePage.style.ts
@@ -16,3 +16,36 @@ export const ImageContainer = styled.div`
     justify-content: center;
   }
 `   
+
+export const DiscoverContainer = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 13rem;
+  margin-left: 3rem;
+  font-size:10rem;
+  font-weight:900;
+  font-style: italic;
+  text-transform: uppercase;
+  /* backdrop-filter: blur(10px); */
+  height:10rem;
+  @media (max-width:1000px) {
+    
+  }
+  @media (max-width:700px) {
+  border: 2px solid;
+  border-radius: 10px;
+  margin-bottom: -20rem;
+  padding: 10px;
+  height: 6rem;
+  backdrop-filter: blur(20px);
+  margin-left:0px;
+  font-size:4rem;
+  font-weight:900;
+  }
+
+  color:rgb(251 191 36);
+  &:hover{
+    color: darkorange;
+  }
+`
