9edf97b4befa9d71169398920b93394c10edc450
n0wherefast <97000096+n0wherefast@users.noreply.github.com>
2024-01-16 00:52:03 +0100

    cleanig code and loading comp

--- app/redux/Provider.tsx
+++ app/redux/Provider.tsx
@@ -13,21 +13,23 @@ function Providers({children}:any) {
   const [load ,setLoad] = useState<boolean>(false)
 
    useEffect(()=>{
-     setTimeout(()=>(setLoad(true)),1000)
+    //  setTimeout(()=>(setLoad(true)),1000)
    },[])
   
   return (
     <Provider store={store}>
      { load === true ? children :
       <div className=' w-full h-screen bg-slate-100 flex flex-col gap-10 justify-center items-center'>
+         <div className=' mt-52 w-[20rem] h-[30rem]'> 
           <div className=' items-center justify-center flex flex-col'>
-            <div className=' font-black italic text-[2.5rem] text-sky-600 uppercase'> BlueWave</div>
-            <div className=' font-black italic text-[2.6rem] text-sky-600 uppercase'> HolidayHouse</div>
+            <div className=' font-black italic lg:text-8xl text-[2.5rem] text-sky-600 uppercase'> BlueWave</div>
+            <div className=' font-black italic lg:text-8xl text-[2.6rem] text-sky-600 uppercase'> HolidayHouse</div>
           </div>
-          <div className=' items-center justify-center flex flex-col absolute top-[11.4rem] left-[1.2rem]'>
-            <div className=' font-black italic text-[2.5rem] text-amber-400 uppercase'> BlueWave</div>
-            <div className=' font-black italic text-[2.6rem] text-amber-400 uppercase'> HolidayHouse</div>
+          <div className=' items-center justify-center flex flex-col relative lg:top-[-11.5rem] top-[-7.4rem] left-[0.2rem]'>
+            <div className=' font-black italic lg:text-8xl text-[2.5rem] text-amber-400 uppercase'> BlueWave</div>
+            <div className=' font-black italic lg:text-8xl text-[2.6rem] text-amber-400 uppercase'> HolidayHouse</div>
           </div>
+        </div>
             
          <Image className=' animate-bounce' src={logo} width={200} height={200} alt='logo image loading'/>
       </div>
