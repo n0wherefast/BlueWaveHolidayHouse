c1266f375235200c7744117b17e7ffabcfaae993
n0wherefast <97000096+n0wherefast@users.noreply.github.com>
2024-01-18 17:38:02 +0100

    updated gallery component

--- app/pages/gallery/page.tsx
+++ app/pages/gallery/page.tsx
@@ -4,14 +4,14 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
-// import '../../globals.css'
+import '../../globals.css'
 import Image from 'next/image';
 import { EffectCoverflow, Pagination ,Autoplay} from 'swiper/modules';
 import img from '../../assets/29-09-11.webp'
 function Gallery() {
   return (
     <>
-        <div className='h-screen w-full pt-[4.5rem]  bg-sky-900' >
+        {/* <div className='h-screen w-full pt-[4.5rem]  bg-sky-900' > */}
         <div>Gallery</div>
         <Swiper
         effect={'coverflow'}
@@ -35,7 +35,7 @@ function Gallery() {
         modules={[EffectCoverflow,Autoplay, Pagination]}
         className="mySwiper pt-[50%] pb-[50%] w-[100%]"
       >
-        <SwiperSlide className=' bg-cover bg-center w-[300px] h-[]'>
+        <SwiperSlide className=' bg-cover bg-center w-[300px] h-[300px]'>
           <Image className=' block w-[100%]'  alt='' width={1000} height={1000} src={img} />
         </SwiperSlide>
         <SwiperSlide>
@@ -63,7 +63,7 @@ function Gallery() {
           <Image className=' block w-[100%]' alt='' width={1000} height={1000} src={img} />
         </SwiperSlide>
       </Swiper>
-        </div>
+        {/* </div> */}
     </>
   )
 }
