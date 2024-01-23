f1755d3cd2ad9d76dc1316dc8132b7fc219c0ea0
n0wherefast <97000096+n0wherefast@users.noreply.github.com>
2024-01-23 01:41:24 +0100

    updated gallery

--- app/pages/gallery/page.tsx
+++ app/pages/gallery/page.tsx
@@ -19,6 +19,7 @@ function Gallery() {
         end:{opacity:1, x:8},
         startG:{opacity:0 ,x:-10},
         endG:{opacity:1, x:1},
+        startE:{opacity:0 ,x:10},
         }
 
     const style ={
@@ -29,13 +30,16 @@ function Gallery() {
      }
   return (
     <>
-        <div className=' flex flex-col justify-start gap-10 items-center min-h-[92vh] w-full pt-[4.5rem] bg-sky-700 ' >
-        <div className='w-full'>   
-            <motion.div variants={variant} initial='startG' whileInView='endG' transition={{delay:0.6}}className="mt-10 absolute   w-[93vw]  text-6xl  font-black italic  border-b-[5rem] border-b-amber-400  border-r-[9rem] border-r-transparent  h-[5.2rem]"/>
-            <motion.div variants={variant} initial='startG' whileInView='endG' transition={{delay:0.8}}className="mt-10  w-[90vw]  text-6xl  font-black italic  border-b-[5rem] border-b-white  border-r-[9rem] border-r-transparent  h-[5.2rem]">
-                {size<700  &&   <motion.h1 variants={variant} initial='start' whileInView='end' transition={{delay:0.7}} className=' p-2 flex items-center justify-center w-full text-amber-400 '>Gallery </motion.h1>}       
-            </motion.div>
-        </div>
+        <div className=' flex flex-col justify-start gap-5 items-center min-h-[92vh] w-full pt-[4.5rem] bg-sky-700 ' >
+                <div className='w-full'>   
+                    <motion.div variants={variant} initial='startG' whileInView='endG' transition={{delay:0.6}}className="mt-10 absolute   w-[93vw]  text-6xl  font-black italic  border-b-[5rem] border-b-amber-400  border-r-[9rem] border-r-transparent  h-[5.2rem]"/>
+                    <motion.div variants={variant} initial='startG' whileInView='endG' transition={{delay:0.8}}className="mt-10  w-[90vw]  text-6xl  font-black italic  border-b-[5rem] border-b-white  border-r-[9rem] border-r-transparent  h-[5.2rem]">
+                        {size<700  &&  <>
+                                        <motion.h1 variants={variant} initial='start' whileInView='end' transition={{delay:0.7}} className=' absolute top-[0.2rem] p-2 flex items-center justify-center w-full text-sky-600 '>Gallery </motion.h1>
+                                        <motion.h1 variants={variant} initial='start' whileInView='end' transition={{delay:0.7}} className=' p-2 flex items-center justify-center w-full text-amber-400 '>Gallery </motion.h1>
+                                    </>}       
+                    </motion.div>
+                </div>
            
          
         <Swiper
@@ -45,7 +49,7 @@ function Gallery() {
             centeredSlides={true}
             slidesPerView={'auto'}
             autoplay={{
-                delay:2500,
+                delay:4500,
                 disableOnInteraction:false
             }}
             coverflowEffect={{
@@ -57,7 +61,7 @@ function Gallery() {
             }}
             pagination={false}
             modules={[EffectCoverflow,Autoplay, Pagination]}
-            // className="w-[100%] pt-[5rem] pb-[5rem]"
+            className=""
       >
             <SwiperSlide  style={style}>
             <Image className=' w-full h-full  rounded-2xl block'  alt='' width={500} height={500} src={img} />
@@ -87,7 +91,17 @@ function Gallery() {
             <Image className=' w-full h-full  rounded-2xl block' alt='' width={500} height={500} src={img} />
             </SwiperSlide>
       </Swiper>
-      <p className='p-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum velit dolores id consequuntur et, eligendi nulla ducimus facilis dicta aliquid.</p>
+
+      <div className='w-full flex flex-row-reverse items-end mr-1'>   
+            <motion.div variants={variant} initial='startE' whileInView='endG' transition={{delay:0.6}}className=" absolute  w-[93vw]  text-6xl  font-black italic  border-b-[5rem] border-b-amber-400  border-l-[9rem] border-l-transparent  h-[5.2rem]"/>
+            <motion.div variants={variant} initial='startE' whileInView='endG' transition={{delay:0.8}}className=" w-[90vw]  text-6xl  font-black italic  border-b-[5rem] border-b-white  border-l-[9rem] border-l-transparent  h-[5.2rem]">
+                   {/* {size<700  &&  <>
+                                  <motion.h1 variants={variant} initial='start' whileInView='end' transition={{delay:0.7}} className=' absolute top-[0.2rem] p-2 flex items-center justify-center w-full text-sky-600 '>Gallery </motion.h1>
+                                  <motion.h1 variants={variant} initial='start' whileInView='end' transition={{delay:0.7}} className=' p-2 flex items-center justify-center w-full text-amber-400 '>Gallery </motion.h1>
+                               </>}         */}
+            </motion.div>
+        </div>
+      {/* <p className='p-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum velit dolores id consequuntur et, eligendi nulla ducimus facilis dicta aliquid.</p> */}
         </div>
     </>
   )
