f4b6aff7bd0f45ee05c103222ae551daf990327f
n0wherefast <97000096+n0wherefast@users.noreply.github.com>
2024-01-30 18:49:41 +0100

    stuff

--- app/pages/gallery/page.tsx
+++ app/pages/gallery/page.tsx
@@ -25,15 +25,15 @@ function Gallery() {
     const style ={
         backgroundSize:'cover',
          backgroundPosition:'center',
-          width: size <= 700? '20rem': size > 700 &&  size <100 ?  '40rem' : '45rem',
-          height: size <= 700? '20rem': size > 700 &&  size <100 ?  '40rem' : '45rem',
+          width: size <= 700? '23rem': size > 700 &&  size <100 ?  '38rem' : '40rem',
+          height: size <= 700? '23rem': size > 700 &&  size <100 ?  '38rem' : '40rem',
      }
   return (
     <>
-        <div className=' flex flex-col justify-center gap-10 items-center min-h-[100vh] w-full pt-[4.5rem] bg-sky-700 ' >
+        <div className=' flex flex-col justify-start gap-5 items-center min-h-screen w-full pt-[4.5rem] bg-sky-700 ' >
                 <div className='w-full'>   
-                    <motion.div variants={variant} initial='startG' whileInView='endG' transition={{delay:0.6}}className="mt-10 absolute   w-[93vw]  text-6xl  font-black italic  border-b-[5rem] border-b-amber-400  border-r-[9rem] border-r-transparent  h-[5.2rem]"/>
-                    <motion.div variants={variant} initial='startG' whileInView='endG' transition={{delay:0.8}}className="mt-10  w-[90vw]  text-6xl  font-black italic  border-b-[5rem] border-b-white  border-r-[9rem] border-r-transparent  h-[5.2rem]">
+                    <motion.div variants={variant} initial='startG' whileInView='endG' transition={{delay:0.6}} className=" absolute   w-[93vw]  text-6xl  font-black italic  border-b-[5rem] border-b-amber-400  border-r-[9rem] border-r-transparent  h-[5.2rem]"/>
+                    <motion.div variants={variant} initial='startG' whileInView='endG' transition={{delay:0.8}} className="  w-[90vw]  text-6xl  font-black italic  border-b-[5rem] border-b-white  border-r-[9rem] border-r-transparent  h-[5.2rem]">
                                     <>
                                         <motion.h1 variants={variant} initial='start' whileInView='end' transition={{delay:0.7}} className=' absolute top-[0.2rem] p-2 flex items-center justify-center w-full text-sky-600 '>Gallery </motion.h1>
                                         <motion.h1 variants={variant} initial='start' whileInView='end' transition={{delay:0.7}} className=' p-2 flex items-center justify-center w-full text-amber-400 '>Gallery </motion.h1>
@@ -64,37 +64,37 @@ function Gallery() {
             className=""
       >
             <SwiperSlide  style={style}>
-            <Image className=' w-full h-full  rounded-2xl block'  alt='' width={500} height={500} src={img} />
+            <Image className=' w-full h-full  rounded-lg block'  alt='' width={500} height={500} src={img} />
             </SwiperSlide>
             <SwiperSlide  style={style} >
-            <Image className=' w-full h-full  rounded-2xl block' alt='' width={500} height={500} src={img} />
+            <Image className=' w-full h-full  rounded-lg block' alt='' width={500} height={500} src={img} />
             </SwiperSlide>
             <SwiperSlide  style={style} >
-            <Image className=' w-full h-full  rounded-2xl block' alt='' width={500} height={500} src={img} />
+            <Image className=' w-full h-full  rounded-lg block' alt='' width={500} height={500} src={img} />
             </SwiperSlide>
             <SwiperSlide  style={style} >
-            <Image className=' w-full h-full  rounded-2xl block' alt='' width={500} height={500} src={img} />
+            <Image className=' w-full h-full  rounded-lg block' alt='' width={500} height={500} src={img} />
             </SwiperSlide>
             <SwiperSlide  style={style} >
-            <Image className=' w-full h-full  rounded-2xl block' alt='' width={500} height={500} src={img} />
+            <Image className=' w-full h-full  rounded-lg block' alt='' width={500} height={500} src={img} />
             </SwiperSlide>
             <SwiperSlide  style={style} >
-            <Image className=' w-full h-full  rounded-2xl block' alt='' width={500} height={500} src={img} />
+            <Image className=' w-full h-full  rounded-lg block' alt='' width={500} height={500} src={img} />
             </SwiperSlide>
             <SwiperSlide  style={style} >
-            <Image className=' w-full h-full  rounded-2xl block' alt='' width={500} height={500} src={img} />
+            <Image className=' w-full h-full  rounded-lg block' alt='' width={500} height={500} src={img} />
             </SwiperSlide>
             <SwiperSlide  style={style} >
-            <Image className=' w-full h-full  rounded-2xl block' alt='' width={500} height={500} src={img} />
+            <Image className=' w-full h-full  rounded-lg block' alt='' width={500} height={500} src={img} />
             </SwiperSlide>
             <SwiperSlide  style={style} >
-            <Image className=' w-full h-full  rounded-2xl block' alt='' width={500} height={500} src={img} />
+            <Image className=' w-full h-full  rounded-lg block' alt='' width={500} height={500} src={img} />
             </SwiperSlide>
       </Swiper>
 
        <div className='w-[100%] flex flex-row-reverse items-end mr-1 lg:mb-5'>   
             <motion.div variants={variant} initial='startE' whileInView='endG' transition={{delay:0.6}}className=" absolute  w-[93vw]  text-6xl  font-black italic  border-b-[5rem] border-b-amber-400  border-l-[9rem] border-l-transparent  h-[5.2rem]"/>
-            <motion.div variants={variant} initial='startE' whileInView='endG' transition={{delay:0.8}}className=" w-[90vw]  text-6xl  font-black italic  border-b-[5rem] border-b-white  border-l-[9rem] border-l-transparent  h-[5.2rem]">
+            <motion.div variants={variant} initial='startE' whileInView='endG' transition={{delay:0.8}} className=" w-[90vw]  text-6xl  font-black italic  border-b-[5rem] border-b-white  border-l-[9rem] border-l-transparent  h-[5.2rem]">
             </motion.div>
         </div>
       {/* <p className='p-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum velit dolores id consequuntur et, eligendi nulla ducimus facilis dicta aliquid.</p> */}
