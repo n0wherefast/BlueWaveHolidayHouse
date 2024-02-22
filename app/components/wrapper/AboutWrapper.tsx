f0c85c5167829f09e970eebe25fbd76ee33bd8ec
n0wherefast <97000096+n0wherefast@users.noreply.github.com>
2024-02-22 20:30:47 +0100

    updated stuff

--- app/components/wrapper/AboutWrapper.tsx
+++ app/components/wrapper/AboutWrapper.tsx
@@ -35,9 +35,9 @@ function AboutWrapper(data:any) {
     <AboutContainer>
         <MainContainer>
         <MainTextContainer className={one.className}>
-            <motion.div variants={variant} initial='startTit' whileInView='endTit' className='flex text-[5rem] md:text-[6.7rem] flex-col lg:flex-row lg:text-[7rem] xl:text-[12rem] lg:gap-5'>La soluzione<p className='text-amber-400 uppercase '>ideale</p></motion.div>
-            <motion.div variants={variant} initial='startRev' whileInView='endRev' className=' text-[5.3rem] md:text-[6.7rem] lg:text-[7rem] xl:text-[10rem] flex-col lg:flex-row w-[90vw] flex lg:gap-5'>Per la tua <p className='text-amber-400 uppercase'>vacanza</p></motion.div>
-            <motion.div variants={variant} initial='startTit' whileInView='endTit' className=' text-[5.3rem] md:text-[6.7rem] lg:text-[7.5rem] xl:text-[12rem]  flex gap-2 lg:gap-5'>In <p className='text-amber-400 uppercase'>Sicilia</p> .</motion.div> 
+            <motion.div variants={variant} initial='startTit' whileInView='endTit' className=' shad flex text-[5rem] md:text-[6.7rem] flex-col lg:flex-row lg:text-[7rem] xl:text-[12rem] lg:gap-5'>La soluzione<p className='text-amber-400 uppercase shadW '>ideale</p></motion.div>
+            <motion.div variants={variant} initial='startRev' whileInView='endRev' className=' shad text-[5.3rem] md:text-[6.7rem] lg:text-[7rem] xl:text-[10rem] flex-col lg:flex-row w-[90vw] flex lg:gap-5'>Per la tua <p className='text-amber-400 shadW uppercase'>vacanza</p></motion.div>
+            <motion.div variants={variant} initial='startTit' whileInView='endTit' className=' shad text-[5.3rem] md:text-[6.7rem] lg:text-[7.5rem] xl:text-[12rem]  flex gap-2 lg:gap-5'>In <p className='text-amber-400 uppercase shadW '>Sicilia</p> .</motion.div> 
         </MainTextContainer>
         <Button to={'#sec'}  y={-15} color={'text-amber-400'}/>
         </MainContainer>
